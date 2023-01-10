705de404ce1ab5a24ae0bcde708a3046705c04de
Jon Bell <github@lot23.com>
2023-01-10 08:02:47 +0000

    Updating

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import rss from "rss";
 const importAll = (context) =>
   context.keys().map((key) => context(key).default);
 const photos = importAll(
-  require.context("../public/gallery/", false, /\.png$/)
+  require.context("../public/gallery/", false, /\.(png|jpe?g|gif)$/)
 ).reverse();
 
 const imgElements = photos.map((photo) => {
@@ -14,29 +14,12 @@ const imgElements = photos.map((photo) => {
     <Image
       src={photo.src}
       className="w-64"
-      width="300"
-      height="300"
+      width={300}
+      height={300}
       key={photo.id}
     />
   );
 });
-
-const feed = new rss({
-  title: "Picadilly",
-  feed_url: "http://example.com/rss.xml",
-  site_url: "http://example.com",
-});
-
-photos.forEach((photo) => {
-  feed.item({
-    title: "A Photo",
-    description: "A beautiful photo",
-    url: photo.src, // Link to the photo
-    date: new Date(), // The current date
-  });
-});
-
-const xml = feed.xml();
 
 export default function Home() {
   return (
