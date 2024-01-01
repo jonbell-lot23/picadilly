b88abfe724371cc8c696f0b25b5be950189192bd
Jon Bell <github@lot23.com>
2024-01-02 09:19:33 +1300

    New ones

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
-import rss from "rss";
+import PhotoAlbum from "react-photo-album";
 
-const importAll = (context) =>
-  context.keys().map((key) => context(key).default);
+const importAll = (context) => {
+  return context.keys().map((key) => {
+    // Retrieve the URL of the image
+    const imageUrl = context(key);
+    const src = typeof imageUrl === "string" ? imageUrl : imageUrl.default;
+
+    return {
+      src,
+      width: 1, // Assign a width aspect (change as needed)
+      height: 1, // Assign a height aspect (change as needed)
+    };
+  });
+};
+
 const photos = importAll(
   require.context("../public/gallery/", false, /\.(png|jpe?g|gif)$/)
-).reverse();
-
-const imgElements = photos.map((photo) => {
-  return <Image src={photo.src} width={300} height={300} key={photo.id} />;
-});
+);
 
 export default function Home() {
   return (
@@ -19,7 +26,19 @@ export default function Home() {
       <Head>
         <title>Picadilly</title>
       </Head>
-      <div className="flex flex-row flex-wrap w-full">{imgElements}</div>
+
+      <PhotoAlbum
+        layout="masonry"
+        photos={photos.map((photo) => ({
+          ...photo,
+          src: photo.src.replace("/_next/static/media/", "/"),
+        }))}
+        columns={(containerWidth) => {
+          if (containerWidth < 500) return 2;
+          if (containerWidth < 900) return 3;
+          return 4;
+        }}
+      />
     </div>
   );
 }
